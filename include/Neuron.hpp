d12808bef9a5c1d828483287b8ee0186ee90de06
Pyccknyn <klucki2003@gmail.com>
2025-01-10 01:47:22 +0100

    added doxygen comments in header

--- include/Neuron.hpp
+++ include/Neuron.hpp
@@ -24,26 +24,71 @@ private:
 
 public:
     /**
-    * @brief Constructs a default Neuron with zero initialization.
-    * Initializes m_value, m_bias, m_activation, m_gradient to 0 and m_weights to an empty vector.
-    */
+     * @brief Default constructor for the Neuron class.
+     * Initializes m_value, m_bias, m_activation, and m_gradient to 0.
+     * Initializes m_weights to an empty vector.
+     */
     Neuron();
 
-    void setValue(double value); ///< Sets the input value.
-    double getValue() const; ///< Gets the input value.
+    /**
+     * @brief Sets the input value of the neuron.
+     * @param value The input value to set.
+     */
+    void setValue(double value); 
 
-    void setBias(double bias); ///< Sets the bias.
-    double getBias() const; ///< Gets the bias.
+    /**
+     * @brief Gets the input value of the neuron.
+     * @return The input value of the neuron.
+     */
+    double getValue() const;
 
-    void setActivation(double activation); ///< Sets the activation value.
-    double getActivation() const; ///< Gets the activation value.
+    /**
+     * @brief Sets the bias of the neuron.
+     * @param bias The bias value to set.
+     */
+    void setBias(double bias); 
 
-    void setGradient(double gradient); ///< Sets the gradient.
-    double getGradient() const; ///< Gets the gradient.
+    /**
+     * @brief Gets the bias of the neuron.
+     * @return The bias of the neuron.
+     */
+    double getBias() const;
 
-    void setWeights(const std::vector<double>& weights); ///< Sets the weights.
-    std::vector<double> getWeights() const; ///< Gets the weights.
+    /**
+     * @brief Sets the activation value of the neuron.
+     * @param activation The activation value to set.
+     */
+    void setActivation(double activation);
 
+    /**
+     * @brief Gets the activation value of the neuron.
+     * @return The activation value of the neuron.
+     */
+    double getActivation() const; 
+
+    /**
+     * @brief Sets the gradient for backpropagation.
+     * @param gradient The gradient value to set.
+     */
+    void setGradient(double gradient); 
+
+    /**
+     * @brief Gets the gradient for backpropagation.
+     * @return The gradient value of the neuron.
+     */
+    double getGradient() const;
+
+    /**
+     * @brief Sets the weights connecting to the previous layer.
+     * @param weights A vector of weights to set.
+     */
+    void setWeights(const std::vector<double>& weights); 
+
+    /**
+     * @brief Gets the weights connecting to the previous layer.
+     * @return A vector of weights of the neuron.
+     */
+    std::vector<double> getWeights() const; 
 
     /**
     * @brief Hyperbolic tangent activation function.
